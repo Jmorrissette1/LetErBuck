b93ccaa40a5bfdf1a89cdce0be03a8c02b811c91
Jason Morrissette <j.morrissette47@gmail.com>
2024-10-17 15:30:58 -0700

    adjusted banner fonts

--- src/app/components/specialsBanner/SpecialsBanner.tsx
+++ src/app/components/specialsBanner/SpecialsBanner.tsx
@@ -13,6 +13,27 @@ const SpecialsBanner: React.FC = () => {
           height={75}
           className={styles.bestValueBtn}
         />
+        <div className={styles.specialsBannersRow}>
+          <div>
+            <Image
+              src="/images/LEB-Logo.svg"
+              alt="let er buck logo"
+              width={110}
+              height={110}
+            />
+          </div>
+          <div>
+            <p className={styles.topLine}>unlimited</p>
+            <p className={styles.middleLine}>Monthly</p>
+            <p className={styles.bottomLine}>Washes</p>
+          </div>
+        </div>
+        <Image
+          src="/banner-arrow.png"
+          alt="blue sign me up button"
+          width={220}
+          height={35}
+        ></Image>
       </div>
     </div>
   );
