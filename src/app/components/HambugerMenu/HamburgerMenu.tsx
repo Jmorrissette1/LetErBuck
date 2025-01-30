c528b441fd44870ac16cf497cd5fc56b673bb0a8
Jason Morrissette <j.morrissette47@gmail.com>
2025-01-30 15:40:03 -0800

    added mobile menu

--- src/app/components/HambugerMenu/HamburgerMenu.tsx
+++ src/app/components/HambugerMenu/HamburgerMenu.tsx
@@ -1,21 +1,29 @@
 "use client";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/app/components/ui/sheet";
 
-import React, { useState } from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/app/components/ui/accordion";
+
 import Link from "next/link";
 import Image from "next/image";
 
 import styles from "./HamburgerMenu.module.css";
 
 const HamburgerMenu: React.FC = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   return (
-    <div className={styles.mobileNav}>
-      <div className="flex-between">
+    <div className="flex">
+      <div>
         <Image
           src="/LEB-Logo.svg"
           alt="Cowboy riding a car"
@@ -23,17 +31,74 @@ const HamburgerMenu: React.FC = () => {
           height={100}
           className={styles.navLogo}
         ></Image>
-        <button onClick={toggleMenu} className={styles.menuButton}>
-          ☰
-        </button>
-        <div
-          className={styles.menuLinks}
-          style={{ display: isMenuOpen ? "block" : "none" }}
-        >
-          <Link href="/packages">Packages</Link>
-          <Link href="/about">About</Link>
-          <Link href="/contact">Contact</Link>
-        </div>
+      </div>
+      <div>
+        <Sheet>
+          <SheetTrigger>Open</SheetTrigger>
+          <SheetContent className={styles.sheetBackground}>
+            <SheetHeader>
+              <SheetTitle>
+                {" "}
+                <Image
+                  src="/LEB-Logo.svg"
+                  alt="Cowboy riding a car"
+                  width={100}
+                  height={100}
+                  className={styles.navLogo}
+                ></Image>
+              </SheetTitle>
+              <SheetDescription></SheetDescription>
+            </SheetHeader>
+            <ul className={styles.navList}>
+              <li className={styles.navListItem}>
+                <Accordion type="single" collapsible>
+                  <AccordionItem value="item-1">
+                    <AccordionTrigger>
+                      <div className={styles.navListItem}>
+                        <Link href="/packages">Packages</Link>
+                      </div>
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      <ul>
+                        <li>
+                          <Link href="#">Wash Packages</Link>
+                        </li>
+                        <li>
+                          <Link href="#">Why Unlimited?</Link>
+                        </li>
+                      </ul>
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
+              </li>
+              <li className={styles.navListItem}>
+                <Link href="/fundraising">Fundraising</Link>
+              </li>
+              <li className={styles.navListItem}>
+                <Accordion type="single" collapsible>
+                  <AccordionItem value="item-2">
+                    <AccordionTrigger>
+                      <div className={styles.navListItem}>
+                        <Link href="/packages">About Us</Link>
+                      </div>
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      <Link href="#">Wash Packages</Link>
+                      <Link href="#">Why Unlimited?</Link>
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
+              </li>
+              <li className={styles.navListItem}>
+                <Link href="/about">Locations</Link>
+              </li>
+
+              <li className={styles.navListItem}>
+                <Link href="/about">Manage Your Subscription</Link>
+              </li>
+            </ul>
+          </SheetContent>
+        </Sheet>
       </div>
     </div>
   );
