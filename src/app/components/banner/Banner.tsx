0c45392ebdfe34c2153896281b6e04a342d9db06
Jason Morrissette <47222019+Jmorrissette1@users.noreply.github.com>
2025-10-07 13:33:50 -0700

    moved bca images

--- src/app/components/banner/Banner.tsx
+++ src/app/components/banner/Banner.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 import Image from "next/image";
 const banner = () => {
     return ( <div>
-        <Link href="#"><Image src="/images/bca/LEB-BCA-D.png" alt="beast cancer awareness 2025" width={3000} height={300} className="banner-img"/></Link>
-        <Link href="#"><Image src="/images/bca/LEB-BCA-M.png" alt="breast cancer awareness 2025"width={360} height={100} className="banner-img-m"/></Link>
+        <Link href="#"><Image src="/images/LEB-BCA-D.png" alt="beast cancer awareness 2025" width={3000} height={300} className="banner-img"/></Link>
+        <Link href="#"><Image src="/images/LEB-BCA-M.png" alt="breast cancer awareness 2025"width={360} height={100} className="banner-img-m"/></Link>
         </div> );
 
 }
